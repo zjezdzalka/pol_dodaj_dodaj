f0887ee1c2b668f5b60d0e0d98cb406306637fdb
Marek Zjeżdżałka <UnderscoreF17@gmail.com>
2024-04-25 22:44:15 +0200

    mniam

---
+++ pol_dodaj_dodaj/library.h
@@ -0,0 +1,53 @@
+#include<iostream>
+#include"numbers.h"
+
+#define uzywaj using
+#define przestrzen_imienna namespace
+#define glowna main
+#define srednik ;
+uzywaj przestrzen_imienna glowna srednik
+
+#define jezeli if
+#define dopoki while
+#define wykonuj do
+#define petla for
+
+
+#define liczba int
+#define krotka_liczba short
+#define dluga_liczba long
+#define uwieszanie float
+#define podwojna double
+
+#define dodaj +=
+
+
+#define wypisz cout
+#define wpisz cin>>
+#define wartosc <<
+#define zwroc return
+
+#define nawias_kwad_l [
+#define nawias_kwad_r ]
+#define nawias_l (
+#define nawias_p )
+#define nawias_was_l {
+#define nawias_was_p }
+
+
+#define wiekszy_od >
+#define wiekszy_rowny_od >=
+#define mniejszy_od <
+#define mniejszy_rowny_od <=
+#define rowny =
+#define czy_rowny ==
+#define rozne_od !=
+#define plus +
+#define minus -
+#define mnozenie *
+#define dzielenie /
+#define reszta_z_dzielenia %
+#define nowa_linia "\n"
+#define spacja " "
+#define kropka .
+#define przecinek ,
