58f6013d02ab503f094cea744cd25e98f9ca1bcc
Marek Zjeżdżałka <UnderscoreF17@gmail.com>
2024-04-28 22:02:22 +0200

    gitignore

---
+++ pol.cpp
@@ -0,0 +1,40 @@
+#include"library.h"
+
+
+uzywaj przestrzen_imienna std srednik
+
+
+liczba euklides nawias_l liczba e przecinek liczba f nawias_p nawias_was_l
+	dopoki nawias_l f rozne_od zero nawias_p nawias_was_l
+		liczba p rowny e reszta_z_dzielenia f srednik
+		e rowny f srednik
+		f rowny p srednik
+	nawias_was_p
+	zwroc e srednik
+nawias_was_p
+
+
+liczba glowna nawias_l nawias_p nawias_was_l
+	wypisz wartosc "podaj liczbe elementow: " srednik
+	liczba a srednik
+	liczba suma rowny zero srednik
+	wpisz a srednik
+	liczba lista nawias_kwad_l a nawias_kwad_r srednik
+	wypisz wartosc "podaj elementy: " srednik
+	petla nawias_l liczba i rowny zero srednik i mniejszy_od a srednik i dodaj jeden nawias_p nawias_was_l
+		wpisz lista nawias_kwad_l i nawias_kwad_r srednik
+		suma dodaj lista nawias_kwad_l i nawias_kwad_r srednik
+	nawias_was_p
+	wypisz wartosc nowa_linia srednik
+	petla nawias_l liczba i rowny zero srednik i mniejszy_od a srednik i dodaj jeden nawias_p nawias_was_l
+		wypisz wartosc lista nawias_kwad_l i nawias_kwad_r wartosc spacja srednik
+	nawias_was_p
+	wypisz wartosc nowa_linia wartosc "suma:" wartosc suma srednik
+	wypisz wartosc nowa_linia wartosc nowa_linia wartosc "wpisz dwie liczby: " srednik
+	liczba e przecinek f srednik
+	wpisz e srednik
+	wpisz f srednik
+	wypisz wartosc "nwd: " wartosc euklides nawias_l e przecinek f nawias_p srednik
+	zwroc zero srednik
+nawias_was_p
+
